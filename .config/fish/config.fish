5ed8ed879f35ffa93424ee47c722be1caa3c8ef1
Thomas (Tom) C. Gorordo <tcgorordo@gmail.com>
2022-07-04 02:47:36 -0700

    fish config

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -2,4 +2,11 @@ if status is-interactive
     # Commands to run in interactive sessions can go here
     set -x GPG_TTY (tty)
     alias dot='git --git-dir=$HOME/.dot/ --work-tree=$HOME'
+    bass source ~/.cargo/env
+    bass source ~/.ghcup/env
+    bass source ~/.elan/env
+
+    if test -f '$HOME/lib/google-cloud-sdk/path.fish.inc'
+        bass source '$HOME/lib/google-cloud-sdk/path.fish.inc'
+    end
 end
